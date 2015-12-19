9c081c4e711709f7cc287cf9c01631ddca4c2435
Joe Li <joe.li@sceneric.com>
2015-12-19 12:13:32 +0800

    To log JDBC parameters： Add log4j.logger.org.hibernate.type to log4j.properties Fixing PluginConfig.java issue: key should not be 'name' Should be 'attributes_key'

--- src/net/eshop/entity/PluginConfig.java
+++ src/net/eshop/entity/PluginConfig.java
@@ -1,7 +1,7 @@
 /*
- * 
- * 
- * 
+ *
+ *
+ *
  */
 package net.eshop.entity;
 
@@ -18,16 +18,18 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
+
 /**
  * Entity - 插件配置
- * 
- * 
- * 
+ *
+ *
+ *
  */
 @Entity
 @Table(name = "t_plugin_config")
 @SequenceGenerator(name = "sequenceGenerator", sequenceName = "t_plugin_config_sequence")
-public class PluginConfig extends OrderEntity {
+public class PluginConfig extends OrderEntity
+{
 
 	private static final long serialVersionUID = -4357367409438384806L;
 
@@ -42,93 +44,105 @@ public class PluginConfig extends OrderEntity {
 
 	/**
 	 * 获取插件ID
-	 * 
+	 *
 	 * @return 插件ID
 	 */
 	@Column(nullable = false, updatable = false, unique = true, length = 100)
-	public String getPluginId() {
+	public String getPluginId()
+	{
 		return pluginId;
 	}
 
 	/**
 	 * 设置插件ID
-	 * 
+	 *
 	 * @param pluginId
-	 *            插件ID
+	 *           插件ID
 	 */
-	public void setPluginId(String pluginId) {
+	public void setPluginId(final String pluginId)
+	{
 		this.pluginId = pluginId;
 	}
 
 	/**
 	 * 获取是否启用
-	 * 
+	 *
 	 * @return 是否启用
 	 */
 	@Column(nullable = false)
-	public Boolean getIsEnabled() {
+	public Boolean getIsEnabled()
+	{
 		return isEnabled;
 	}
 
 	/**
 	 * 设置是否启用
-	 * 
+	 *
 	 * @param isEnabled
-	 *            是否启用
+	 *           是否启用
 	 */
-	public void setIsEnabled(Boolean isEnabled) {
+	public void setIsEnabled(final Boolean isEnabled)
+	{
 		this.isEnabled = isEnabled;
 	}
 
 	/**
 	 * 获取属性
-	 * 
+	 *
 	 * @return 属性
 	 */
 	@ElementCollection(fetch = FetchType.EAGER)
 	@CollectionTable(name = "t_plugin_config_attribute")
-	@MapKeyColumn(name = "name", length = 100)
-	public Map<String, String> getAttributes() {
+	@MapKeyColumn(name = "attributes_key", length = 100)
+	public Map<String, String> getAttributes()
+	{
 		return attributes;
 	}
 
 	/**
 	 * 设置属性
-	 * 
+	 *
 	 * @param attributes
-	 *            属性
+	 *           属性
 	 */
-	public void setAttributes(Map<String, String> attributes) {
+	public void setAttributes(final Map<String, String> attributes)
+	{
 		this.attributes = attributes;
 	}
 
 	/**
 	 * 获取属性值
-	 * 
+	 *
 	 * @param name
-	 *            属性名称
+	 *           属性名称
 	 * @return 属性值
 	 */
 	@Transient
-	public String getAttribute(String name) {
-		if (getAttributes() != null && name != null) {
+	public String getAttribute(final String name)
+	{
+		if (getAttributes() != null && name != null)
+		{
 			return getAttributes().get(name);
-		} else {
+		}
+		else
+		{
 			return null;
 		}
 	}
 
 	/**
 	 * 设置属性值
-	 * 
+	 *
 	 * @param name
-	 *            属性名称
+	 *           属性名称
 	 * @param value
-	 *            属性值
+	 *           属性值
 	 */
 	@Transient
-	public void setAttribute(String name, String value) {
-		if (getAttributes() != null && name != null) {
+	public void setAttribute(final String name, final String value)
+	{
+		if (getAttributes() != null && name != null)
+		{
 			getAttributes().put(name, value);
 		}
 	}
